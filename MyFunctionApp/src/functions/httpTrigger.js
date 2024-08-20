f35411de68abdbad0c1111bc2ee59e1c07733e14
mercyjacob9 <107107582+mercyjacob9@users.noreply.github.com>
2024-08-20 19:23:26 +0530

    DB

--- MyFunctionApp/src/functions/httpTrigger.js
+++ MyFunctionApp/src/functions/httpTrigger.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg');
 
 // Create a pool of clients to manage connections
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+    user: 'dbuser',
+    host: 'nestit-337',
+    database: 'test',
+    password: 'dbuser',
+    port: 5432,
 });
 
 app.http('httpTrigger', {
