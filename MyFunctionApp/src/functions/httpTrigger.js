0a90ddb05ab27b4b932f8fb564792517f67089d8
mercyjacob9 <107107582+mercyjacob9@users.noreply.github.com>
2024-08-20 19:43:19 +0530

    change

--- MyFunctionApp/src/functions/httpTrigger.js
+++ MyFunctionApp/src/functions/httpTrigger.js
@@ -1,33 +1,13 @@
 const { app } = require('@azure/functions');
-const { Pool } = require('pg');
-
-// Create a pool of clients to manage connections
-const pool = new Pool({
-    user: 'dbuser',
-    host: 'nestit-337',
-    database: 'test',
-    password: 'dbuser',
-    port: 5432
-});
 
 app.http('httpTrigger', {
-  methods: ['GET', 'POST'],
-  authLevel: 'anonymous',
-  handler: async (request, context) => {
-    try {
-      // Query the database
-      const result = await pool.query('SELECT * FROM sst_work_order');
-  
-      // Send the result as a response
-      context.res = {
-        status: 200,
-        body: result.rows
-      };
-    } catch (error) {
-      context.res = {
-        status: 500,
-        body: error.message
-      };
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
     }
-  }
 });
